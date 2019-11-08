a9759b26d30124b594ee5efdde57f83cd21d43a4
Niracler <1026037967@qq.com>
2019-11-08 14:45:18 +0800

    Add verbose_name

--- movie/models.py
+++ movie/models.py
@@ -5,19 +5,18 @@ from django.db import models
 
 
 class Movie(models.Model):
-    mid = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=65)
-    english_name = models.CharField(max_length=128)
-    release_year = models.CharField(max_length=16)
-    default_image = models.CharField(max_length=256)
-    box_office = models.IntegerField()
-    area = models.CharField(max_length=128)
-    area_id = models.IntegerField()
-    ranking = models.IntegerField()
-    created = models.DateTimeField()
+    mid = models.IntegerField(primary_key=True, verbose_name='ID')
+    name = models.CharField(max_length=65, verbose_name='电影名称')
+    english_name = models.CharField(max_length=128, verbose_name='英文名称')
+    release_year = models.CharField(max_length=16, verbose_name='发行年限')
+    default_image = models.CharField(max_length=256, verbose_name='图片')
+    box_office = models.IntegerField(verbose_name='票房')
+    area = models.CharField(max_length=128, verbose_name='地区')
+    area_id = models.IntegerField(verbose_name='地区号')
+    ranking = models.IntegerField(verbose_name='排名')
+    created = models.DateTimeField(verbose_name='爬取时间')
 
     class Meta:
-        ordering = ('-created',)
         db_table = "movie"
 
     def __str__(self):
